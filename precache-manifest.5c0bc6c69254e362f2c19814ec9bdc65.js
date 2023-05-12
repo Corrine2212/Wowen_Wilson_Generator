67bbe6bfdc4b782b6f6b998bb90c4b039f8957d3
Corrine <corrine.sing@gmail.com>
2023-05-12 11:00:29 +0100

    Updates

---
+++ precache-manifest.5c0bc6c69254e362f2c19814ec9bdc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20aee0e703512ed829eb",
+    "url": "/wowen_wilson_generator/static/css/main.1fdc8fc6.chunk.css"
+  },
+  {
+    "revision": "20aee0e703512ed829eb",
+    "url": "/wowen_wilson_generator/static/js/main.e36ed30d.chunk.js"
+  },
+  {
+    "revision": "8bea05aabf8562f69de6",
+    "url": "/wowen_wilson_generator/static/js/runtime~main.158b4dc5.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/wowen_wilson_generator/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/wowen_wilson_generator/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "616506a4b98d278f17fa438b9cd81bf7",
+    "url": "/wowen_wilson_generator/index.html"
+  }
+];
