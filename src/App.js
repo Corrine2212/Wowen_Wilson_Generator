3c2be5e207ddf7dc9a488be94d0d9614a8a3738d
Corrine <corrine.sing@gmail.com>
2023-05-12 11:04:41 +0100

    fixed error in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header>
         {/* <h1>Wowen Wilson</h1> */}
-        <a href=''><img className='logo' src="./1.png" alt="logo"></img></a>
+        <a href='/'><img className='logo' src="./1.png" alt="logo"></img></a>
       </header>
       <p id='description'>Get ready to experience the ultimate Owen Wilson “wow” extravaganza! This website is dedicated to all the times the actor has famously exclaimed his signature catchphrase.  With our API generator, you can now enjoy an endless supply of ‘wows’ that are sure to entertain and amuse. Whether you’re a fan of his movies or just love the meme-worthy catchphrase, we have something for everyone. So brace yourself for a wild ride and lets get wowing!</p>
 
